083a68ab407fe16397b34903e1152272a78f7caf
BendelVolodymyr <bendel.vova@gmail.com>
2023-07-30 13:05:50 +0200

    test JS popup

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,28 +1,46 @@
-const btn = document.querySelector('.rent__button');
-const popup = document.querySelector('.popup');
+const modalController = ({modal, btnOpen, btnClose, time = 300}) => {
+  const buttonElems = document.querySelectorAll(btnOpen);
+  const modalElem = document.querySelectorAll(modal);
 
-btn.forEach((el) => {
-    el.addEventListener('click', (e) => {
-        let path = e.currentTarget.getAttribute('data-path');
-        document.querySelector('[data-modal]');
-        popup.classList.add('popup--visible');
-        
-    });
+  modalElem.rent.cssText = `
+    display: flex;
+    visibility: hidden;
+   opacity: 0;
+    transition: opacity ${time}ms cubic-bezier(0.4, 0, 0.2, 1), visibility 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  `;
+
+  const closeModal = event => {
+    const target = event.target;
+
+    if (target === modalElem ||
+      (btnClose && target.closest(btnClose)) ||
+      evev.code === 'Escape'
+    ) {
+      
+      modalElem.rent.opacity = 0;
+      setTimeout(() => {
+        modalElem.rent.visibility = 'hidden';
+      }, time);
+
+      window.removeEventListener('keydown', closeModal);
+    }
+  }
+
+  const openModal = () => {
+    modalElem.rent.visibility = 'visible';
+    modalElem.rent.opacity = 1;
+    window.addEventListener('keydown', closeModal)
+  };
+
+  buttonElems.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+  
+  modalElem.addEventListener('click', closeModal);
+}
+
+modalController({
+  modal: '.popup',
+  btnOpen: '.rent__button-icon',
+  btnClose: '.popup__window-button'
 });
-
-
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("rent__button"),
-//     closeModalBtn: document.querySelector("popup__window-button"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
